568867b97b9d99ae8b441fd2ecf5989ac52e9327
HPreast <preasths1@gmail.com>
2021-06-09 14:15:48 -0500

    Implemented appropriate PerformSkill methods to each crew member class

--- Hacker.cs
+++ Hacker.cs
@@ -4,12 +4,17 @@ namespace Heist2
 {
     public class Hacker : IRobber
     {
-        public string name { get; set; }
+        public string Name { get; set; }
         public int SkillLevel { get; set; }
         public int PercentageCut { get; set; }
         public void PerformSkill(Bank bank)
         {
-
+            bank.AlarmScore -= SkillLevel;
+            Console.WriteLine($"{Name} is hacking the alarm system. Security has been decreased by {SkillLevel} points.");
+            if (bank.AlarmScore == 0)
+            {
+                Console.WriteLine($"{Name} has disabled the alarm system!");
+            }
         }
     }
 }
--- LockSpecialist.cs
+++ LockSpecialist.cs
@@ -4,12 +4,17 @@ namespace Heist2
 {
     public class LockSpecialist : IRobber
     {
-        public string name { get; set; }
+        public string Name { get; set; }
         public int SkillLevel { get; set; }
         public int PercentageCut { get; set; }
         public void PerformSkill(Bank bank)
         {
-
+            bank.VaultScore -= SkillLevel;
+            Console.WriteLine($"{Name} is breaking into the vault. The vault's security has been decreased by {SkillLevel} points.");
+            if (bank.VaultScore == 0)
+            {
+                Console.WriteLine($"{Name} has cracked the safe!");
+            }
         }
     }
 }
